9f515aded0a68d0a3f9f3aa86cab8b5f9ee14777
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-04-01 23:36:03 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,12 +12,12 @@ class Patient
     @@all << self
   end
 
-  def self.all
-    @@all
-  end
-
   def new_appointment(doctor, date)
     appointment = Appointment.new(date, self, doctor)
+  end
+
+  def self.all
+    @@all
   end
 
   def appointments
